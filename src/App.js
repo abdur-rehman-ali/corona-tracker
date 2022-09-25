b651c5fd505696dc243126003252ab5217c00e74
abdur-rehman-ali <alijoyia911@gmail.com>
2022-09-26 01:35:54 +0500

    basic setup of project directory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import {Chart, Cards, CountryPicker} from './components'
+import styles from './App.module.css'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello corona tracker</h1>
+      <div className={styles.container}>
+        <Chart/>
+        <Cards/>
+        <CountryPicker/>
       </div>
     )
   }
