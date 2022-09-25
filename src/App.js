e366e97ddaa02d4f3dab389edc901c15ddee1435
abdur-rehman-ali <alijoyia911@gmail.com>
2022-09-26 02:07:27 +0500

    Api file to fetchdata

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 import {Chart, Cards, CountryPicker} from './components'
 import styles from './App.module.css'
+import { fetchData } from './api'
 
 export default class App extends Component {
+  async componentDidMount(){
+    const data = await fetchData()
+    console.log(data);
+  }
   render() {
     return (
       <div className={styles.container}>
