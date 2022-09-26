14bccdc521daa5999225cae5cf4fb25ff0bb03fb
abdur-rehman-ali <alijoyia911@gmail.com>
2022-09-26 13:11:16 +0500

    Display API data in cards component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const Cards = ({data}) => {
-  console.log(data);
+const Cards = ({ data: { confirmed, deaths, recovered, lastUpdate } }) => {
+  if (!confirmed) {
+    return;
+  }
   return (
-    <div>Cards</div>
-  )
-}
+    <div>
+      <div>
+        <h1>Display confirmed</h1>
+        <p>Confirmed: {confirmed.value}</p>
+        <span>{new Date(lastUpdate).toDateString()}</span>
+      </div>
+      <div>
+        <h1>Display deaths</h1>
+        <p>Deaths: {deaths.value}</p>
+        <span>{new Date(lastUpdate).toDateString()}</span>
+      </div>
+      <div>
+        <h1>Display recoverd</h1>
+        <p>Recoverd: {recovered.value}</p>
+        <span>{new Date(lastUpdate).toDateString()}</span>
+      </div>
+    </div>
+  );
+};
 
-export default Cards
+export default Cards;
